bac95b314d2e160782c3c9977cd55887e1820ce5
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-07-22 05:29:19 +0800

    Added AboutMe section along with styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,24 +1,27 @@
 export default function AboutMe() {
   return (
-    <div className="grid auto-rows-5 off-white w-[240px] h-[649px] bg-cream justify-center">
+    <div className="grid auto-rows-5 off-white w-[240px] h-[649px] bg-cream justify-center shadow aboutMe">
       <img
         src="/images/profile-photo.jpg"
         alt="profile-photo"
-        className="w-[140px] h-[140px] rounded-full object-cover border-2 border-[d9d9d9]"
+        className="w-[140px] h-[140px] rounded-full object-cover border-2 border-white mx-auto"
       />
-      <img
-        src="/icons/clover.svg"
-        alt="clover-icon"
-        className="w-[40px] h-[40px]"
-      />
-      <h1>
-        About <br /> Me
-      </h1>
-      <p>
-        Hi, I’m Anne. Combining my tech experience with modern web tools, I
-        build responsive, user-friendly websites using HTML, CSS, JavaScript,
-        and React. I enjoy turning ideas into functional and accessible web
-        applications.
+      <div>
+        <img
+          src="/icons/clover.svg"
+          alt="clover-icon"
+          className="w-[50px] h-[50px]"
+        />
+        <h1 className="lora-bold charcoal-text leading-snug">
+          About <br /> Me
+        </h1>
+      </div>
+
+      <p className="charcoal-text">Hi, I’m Anne.</p>
+      <p className="charcoal-text">
+        Combining my tech experience with modern web tools, I build responsive,
+        user-friendly websites using HTML, CSS, JavaScript, and React. I enjoy
+        turning ideas into functional and accessible web applications.
       </p>
     </div>
   );
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,6 @@
 export default function Body() {
   return (
-    <div className="grid auto-rows-5 off-white w-[640px] h-[1552px] bg-cream justify-center">
+    <div className="grid auto-rows-5 off-white w-[640px] h-[1552px] bg-cream justify-center shadow">
       <h1 className="text-black"> Main Body here</h1>
     </div>
   );
