4cec6f300e147812a4c0893f67de352b88f15653
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-07-23 07:12:36 +0800

    Fixed wrappers for the sections and buttons

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,18 +2,25 @@ import ProjectCard from "./ProjectCard";
 
 export default function Body() {
   return (
-    <div className="off-white bg-cream charcoal-text flex h-[1552px] w-[640px] flex-col py-10 pl-16 font-cg font-medium shadow">
-      <h1 className="mb-4">Projects</h1>
-      <div className="flex flex-col gap-8">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+    <div className="off-white bg-cream charcoal-text h-auto w-[640px] pb-20 font-cg font-medium shadow">
+      <div className="flex flex-col py-10 pl-16">
+        <h1 className="mb-4">My Projects</h1>
+        <div className="ml-[39px] flex flex-col gap-8">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+        <h1 className="mb-4 mt-20">Skills</h1>
+        {/* create props for section headers for mapping */}
+        <h1 className="mb-4 mt-20">Let's Connect</h1>
+        <h1 className="mb-4 mt-20">Craft & Dedication</h1>
       </div>
-      <h1 className="mb-4 mt-20">Skills</h1>
-      {/* create props for section headers for mapping */}
-      <h1 className="mb-4 mt-20">Let's Connect</h1>
-      <h1 className="mb-4 mt-20">Craft & Dedication</h1>
-      <button>Click for a Zen Moment</button>
+      <div className="mt-6 flex justify-center">
+        <button className="bg-accent w-[289px] rounded-[5px] py-[6px] font-cg text-[#FFFFFF]">
+          {" "}
+          Click for a Zen Moment
+        </button>
+      </div>
     </div>
   );
 }
