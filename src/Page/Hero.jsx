e755591444831ffe55bc1e89d2e759505a0cf90a
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-07-25 03:07:16 +0800

    refactor: refactored hardcoded Skills to mapping for clean code
    - added dual map function to map categories and ff skills
    - added css pseudo styling for skills
    - added projects.json file for myprojects data
    - fixed from class to className in hero.jsx

--- src/Page/Hero.jsx
+++ src/Page/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div class="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center">
       <div className="bg-cream mx-auto mt-5 flex h-[249px] w-[650px] flex-col justify-center px-13 pb-10 pt-16 font-jacques shadow">
         <h1 className="charcoal-text text-[40px]">Hi, I’m Anne.</h1>
         <h2 className="charcoal-text mt-1 text-xl">
--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,43 +1,25 @@
+import skillsData from "../data/skillsData.json";
+
 export default function Skills() {
   return (
     <div>
-      {/* map projtitle here */}
-      <div className="mb-5 ml-5">
-        <p className="mb-1 font-semibold">Core Skills: </p>
-        <p className="ml-5">
-          Problem-Solving · Debugging & Troubleshooting · Code Refactoring &
-          Optimization · Requirements Analysis · Adaptability
-        </p>
-      </div>
-      {/* remove this part when starting with mapping */}
-      <div className="mb-5 ml-5">
-        <p className="mb-1 font-semibold">Languages: </p>
-
-        <p className="ml-5">JavaScript (ES6+) · HTML5 · CSS</p>
-      </div>
-
-      <div className="mb-5 ml-5">
-        <p className="mb-1 font-semibold">Frameworks & Libraries: </p>
-
-        <p className="ml-5">ReactJS · Bootstrap · Tailwind CSS (basic)</p>
-      </div>
-
-      <div className="mb-5 ml-5">
-        <p className="mb-1 font-semibold">Version Control: </p>
-
-        <p className="ml-5"> Git (branching · merging · pull requests)</p>
-      </div>
-
-      <div className="mb-5 ml-5">
-        <p className="mb-1 font-semibold">API Integration: </p>
-        <p className="ml-5">RESTful APIs (JSON fetching & dynamic UI)</p>
-      </div>
-
-      <div className="mb-5 ml-5">
-        <p className="mb-1 font-semibold">Tools:</p>
-        <p className="ml-5">
-          Visual Studio Code · npm · Oracle RightNow Service Cloud · Powershell
-        </p>
+      <div className="mb-10 ml-5">
+        {skillsData.map((category) => (
+          <div key={category.id} id={category.id}>
+            <p className="mb-1 font-bold">{category.title}: </p>
+            <p className="mb-5 ml-5">
+              {category.skills.map((individualSkill) => (
+                <span
+                  key={individualSkill}
+                  id={individualSkill.replace(/\s+/g, "-").toLowerCase()}
+                  className="skill-item"
+                >
+                  {individualSkill}
+                </span>
+              ))}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
