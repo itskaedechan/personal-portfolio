484994356caad0bdedde75b7045a3afdf553edba
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-08-11 20:34:53 +0800

    feat: changed mailto to email for copy function for easier email contact"

--- src/components/sections/LetsConnect.jsx
+++ src/components/sections/LetsConnect.jsx
@@ -1,7 +1,16 @@
 import contacts from "../data/contacts.json";
 import Button from "../buttons/Button";
+import { useState } from "react";
 
 export default function LetsConnect() {
+  const [copiedId, setCopiedId] = useState(null);
+
+  const copyEmail = (email, id) => {
+    navigator.clipboard.writeText(email);
+    setCopiedId(id);
+    setTimeout(() => setCopiedId(null), 2000);
+  };
+
   return (
     <div id="contact" className="mt-3 flex flex-col justify-center space-y-6">
       <div className="flex justify-center gap-6 sm:gap-8 md:gap-12">
@@ -12,18 +21,25 @@ export default function LetsConnect() {
             rel="noopener noreferrer"
             key={social.id}
             className="group relative flex"
+            onClick={(e) => {
+              if (social.type === "email") {
+                e.preventDefault();
+                copyEmail(social.link, social.id);
+              }
+            }}
           >
             <img
               src={social.img}
               alt={social.social}
               className="transition-transform duration-200 ease-in-out hover:scale-110"
             />
-            <span className="text-md absolute left-10 top-9 rounded-md bg-gray-100 px-2 py-1 opacity-0 shadow transition duration-200 group-hover:opacity-100">
-              {social.hover}
+            <span className="text-md absolute left-10 top-9 whitespace-nowrap rounded-md bg-gray-100 px-2 py-1 opacity-0 shadow transition duration-200 group-hover:opacity-100">
+              {copiedId === social.id ? "Email copied!" : social.hover}
             </span>
           </a>
         ))}
       </div>
+
       <Button
         variant="resume"
         href="https://bit.ly/4m8UWXS"
